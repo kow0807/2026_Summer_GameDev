c69b204dee0c2f63f23f55a559f9fdfe2aac5b83
ASOST\2316031 <hongyue603@gmail.com>
2026-04-22 13:28:09 +0900

    Add New Select Game

--- MiniGame_BaseProject/Src/Scene/GameScene.h
+++ MiniGame_BaseProject/Src/Scene/GameScene.h
@@ -11,7 +11,10 @@ public:
 
 	// 定数
 	// ----------------------------
-	
+
+	// ミニゲーム数
+	static constexpr int M_MAX_SELECT = 8;
+
 	// ----------------------------
 
 	// コンストラクタ
@@ -27,4 +30,12 @@ public:
 
 private:
 
+	// ミニゲーム選択状態
+	int mSelectIndex_;
+
+	// ミニゲーム選択操作
+	void SelectGameUpdate(void);
+
+	// ミニゲーム選択UI
+	void SelectGameDrawUI(void);
 };
